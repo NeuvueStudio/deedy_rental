ee01bf255430b8036bdd46793d8620d365117ea7
NeuvueStudio <info@neuvuestudio.com>
2025-10-02 14:53:55 +0530

    initial Commit

--- app/Models/ContentMeta.php
+++ app/Models/ContentMeta.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ContentMeta extends Model
 {
-    //
+    use HasFactory;    
+
+    protected $table = 'content_meta';
+    protected $fillable = ['content_id', 'meta_key', 'meta_value'];
+
+    // Relationship: Meta belongs to one content
+    public function content()
+    {
+        return $this->belongsTo(Content::class, 'content_id');
+    }
 }
